a6a965011fd67c8bbb7df03b5fa20f510ba52eea
Moulart Vladimir <moulartvladimir@icloud.com>
2025-06-17 23:53:30 +0200

    fix bugs + tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,9 @@
-const nextJest = require("next/jest")
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
-})
+});
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
@@ -13,7 +11,9 @@ const customJestConfig = {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   testMatch: ["**/__tests__/**/*.test.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
-}
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // Ajouter cette ligne pour transformer le code
+  },
+};
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
